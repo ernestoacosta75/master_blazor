de8594f378fbec309fc4c39c81a2e29ff6814c05
Ernesto Antonio Rodriguez Acosta <ernestoantonio.rodriguezacosta@msc.com>
2025-02-18 12:11:37 +0100

    Manejo de errores

--- FundamentosBlazor/Components/Pages/ErrorManagement.razor.cs
+++ FundamentosBlazor/Components/Pages/ErrorManagement.razor.cs
@@ -2,5 +2,22 @@
 {
     public partial class ErrorManagement
     {
+        private string _errorMessage = string.Empty;
+        private bool _hasError = false;
+
+        public void ErrorTriggered()
+        {
+            try
+            {
+                throw new InvalidOperationException("This is an error controlled!!");
+            }
+            catch (Exception ex)
+            {
+                _hasError = true;
+                _errorMessage = ex.Message;
+
+                throw;
+            }
+        }
     }
 }
